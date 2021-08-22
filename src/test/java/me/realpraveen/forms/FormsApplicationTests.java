fdcc464e87b2fac5eb691ba00ea7ceb58ef4c3d6
jayanpraveen <jayanpraveen30@gmail.com>
2021-08-22 17:42:23 +0530

    inital commit

---
+++ src/test/java/me/realpraveen/forms/FormsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.realpraveen.forms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
